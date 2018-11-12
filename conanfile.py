21514a588d069ccee788c065b3481015d76b8ce0
cppbitman <chinadaihongjun@gmail.com>
2018-11-12 00:44:06 -0800

    update way of getting source with submodules

--- conanfile.py
+++ conanfile.py
@@ -18,11 +18,20 @@ class GsteditingservicesConan(ConanFile):
                 "gst-plugins-bad-1.0/1.14.4@conanos/dev")
     
     source_subfolder = "source_subfolder"
+    remotes = {'origin': 'https://github.com/GStreamer/gst-editing-services.git'}
 
     def source(self):
-        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
-        extracted_dir = "gst-editing-services-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        #tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
+        #extracted_dir = "gst-editing-services-" + self.version
+        #os.rename(extracted_dir, self.source_subfolder)
+        tools.mkdir(self.source_subfolder)
+        with tools.chdir(self.source_subfolder):
+            self.run('git init')
+            for key, val in self.remotes.items():
+                self.run("git remote add %s %s"%(key, val))
+            self.run('git fetch --all')
+            self.run('git reset --hard %s'%(self.version))
+            self.run('git submodule update --init --recursive')
 
     def build(self):
         with tools.chdir(self.source_subfolder):
